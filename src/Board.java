dfbf586637c4afdef42a7d2259585f2e124cc529
manueltov <34443692+manueltov@users.noreply.github.com>
2019-03-10 10:03:37 +0000

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -5,7 +5,7 @@
  */
 public class Board {
 
-    private Piece[][] board;
+    private char[][] board;
 
     /**
      * Construtor de Board
@@ -14,17 +14,40 @@ public class Board {
      * tabuleiro de Pecas de 5 por 4
      */
     public Board() {
-        board = new Piece[5][4];
+        board = new char[5][4];
     }
 
     /**
      * Metodo para adicionar pecas ao tabuleiro
      *
      * @param piece - peca a adicionar
-     * @return  Retorna 1 caso peca tenha sido adicionada corretamente
+     * @return  Retorna 0 caso peca tenha sido adicionada corretamente
      *          Retorna -1 caso contrario
      */
-    private int addPiece (Piece piece){
+    private int addPiece (Piece piece, int x, int y){
+        if(Utilities.pecaEncaixaTabuleiro(piece, x, y)){
+            board[x][y] = piece.getType();
+            return 0;
+        }
+        else{
+            return -1;
+        }
+    }
 
+    /**
+     * Metodo para remover pecas ao tabuleiro
+     *
+     * @param piece - peca a remover
+     * @return  Retorna 0 caso peca tenha sido removida corretamente
+     *          Retorna -1 caso contrario
+     */
+    private int removePiece (Piece piece){
+        if(Utilities.pecaEncaixaTabuleiro(piece, x, y)){
+            board[x][y] = piece.getType();
+            return 0;
+        }
+        else{
+            return -1;
+        }
     }
 }
