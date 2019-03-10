7626dbf3b190ca01ba496c6c31e738d7f65c3095
manueltov <34443692+manueltov@users.noreply.github.com>
2019-03-10 15:59:36 +0000

    Update Game.java
    uns metodos da Classe Board para a Class Game

--- src/Game.java
+++ src/Game.java
@@ -11,5 +11,35 @@ public class Game {
         board = Utilities.loadLevel(level);
     }
 
+    /**
+     * Metodo para adicionar pecas ao tabuleiro
+     *
+     * @param piece - peca a adicionar
+     * @return Retorna 0 caso peca tenha sido adicionada corretamente
+     * Retorna -1 caso contrario
+     */
+    private int addPiece(Piece piece, int x, int y) {
+        if (Utilities.pecaEncaixaTabuleiro(piece, x, y)) {
+            board.board[x][y] = piece.getType();
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Metodo para remover pecas ao tabuleiro
+     *
+     * @param piece - peca a remover
+     * @return Retorna 0 caso peca tenha sido removida corretamente
+     * Retorna -1 caso contrario
+     */
+    private int removePiece(Piece piece) {
+        if (Utilities.existePecaTabuleiro(piece)) {
+            board.board[piece.getX()][piece.getY()] = '\0';
+            return 0;
+        } else
+            return -1;
+    }
 
 }
