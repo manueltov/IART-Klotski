9ee3236ad2fdf59e2f2a2a11905dd1928dd1e03c
manueltov <34443692+manueltov@users.noreply.github.com>
2019-03-10 10:45:24 +0000

    Update Utilities.java
    TODO method para carregar level

--- src/Utilities.java
+++ src/Utilities.java
@@ -4,8 +4,9 @@
 public class Utilities {
 
     // TODO
-    // algo q veriica se nas coordenadas dadas ha espaco para adicionar especificamente a peca que foi dada
-    public static boolean pecaEncaixaTabuleiro (Piece piece, int x, int y){
+    // algo q veriica se nas coordenadas dadas ha espaco para adicionar
+    // especificamente a peca que foi dada
+    public static boolean pecaEncaixaTabuleiro(Piece piece, int x, int y) {
         return true;
     }
 
@@ -13,5 +14,12 @@ public class Utilities {
     // algo q veriica se existe a peca dada no atual tabuleiro
     public static boolean existePecaTabuleiro(Piece piece) {
         return true;
+    }
+
+    // TODO
+    // um metodo que apos ler ficheiro vai carregar as varias pecas desse nivel
+    // para um tabueleiro e devolve esse tabuleiro jah com as pecas
+    public static Board loadLevel(int level) {
+        return null;
     }
 }
