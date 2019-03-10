18cc4aee313fb1fd4d9772ed3e26dabac99f217b
manueltov <34443692+manueltov@users.noreply.github.com>
2019-03-10 10:31:35 +0000

    Create Game.java

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+/**
+ * Esta classe eh que eh a do Jogo
+ * Esta cria um tabuleiro e colaca pecas nele
+ * E gere as jogadas
+ */
+public class Game {
+
+    public Game() {
+        Board board = new Board();
+    }
+
+
+}
