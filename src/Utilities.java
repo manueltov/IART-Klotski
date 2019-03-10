174bc58454e1b2087b87aa0e3e2bb3e34f564fbe
manueltov <34443692+manueltov@users.noreply.github.com>
2019-03-10 10:03:33 +0000

    Create Utilities.java

---
+++ src/Utilities.java
@@ -0,0 +1,11 @@
+/**
+ * Esta classe tem metodos uteis para as restantes classes
+ */
+public class Utilities {
+
+    // TODO
+    // algo q veriica se nas coordenadas dadas ha espaco para adicionar especificamente a peca que foi dada
+    public static boolean pecaEncaixaTabuleiro (Piece piece, int x, int y){
+        return true;
+    }
+}
