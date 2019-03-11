7289a11549c306fdf70ca46779deca18fed7df55
manueltov <34443692+manueltov@users.noreply.github.com>
2019-03-11 13:02:38 +0000

    Update Board.java
    voltar a passar de char[][] para Piece[][]

--- src/Board.java
+++ src/Board.java
@@ -4,15 +4,14 @@
  */
 public class Board {
 
-    public char[][] board;
+    public Piece[][] board;
 
     /**
      * Construtor de Board
-     * <p>
      * Ao crir um novo Board, eh inicializado um
      * tabuleiro de Pecas de 5 por 4
      */
     public Board() {
-        board = new char[5][4];
+        board = new Piece[5][4];
     }
 }
